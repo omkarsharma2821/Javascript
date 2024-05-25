d885c4d04de4670c33ca350ec834e0ee20581b02
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-25 11:13:33 +0530

    NaN concept

---
+++ NaN.js
@@ -0,0 +1,8 @@
+let score = "33abc"
+console.log(typeof score);
+
+// conversion
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // the value in this is NaN 
