fc2a8545dde98364b7617889c5a8f8b59f3317ac
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-25 11:00:52 +0530

    datatypes in js

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+// null => standalone value
+// undefined => value is not assigned
+// number
+// string
+// boolean
+// obejct
+
+let age = null;
+let name;
+console.log(typeof age)
+console.log(typeof name);
