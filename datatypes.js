96a594e1257165a7ceb97f191e669153f020346f
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-25 11:03:21 +0530

    datatypes in js

--- datatypes.js
+++ datatypes.js
@@ -8,4 +8,8 @@
 let age = null;
 let name;
 console.log(typeof age)
+// type of null => object
 console.log(typeof name);
+// type of undefined => undefined
+console.log(typeof null)
+console.log(typeof undefined)
