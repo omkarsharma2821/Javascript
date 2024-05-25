9cfce1c6310d50afd809d69da119ac8c456ab335
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-25 10:45:30 +0530

    variables in js

--- variable.js
+++ variable.js
@@ -1,11 +1,19 @@
 const accountID = 12345;
 let accountEmail = "abc@gmail.com";
 var accountPassword = "1234";
+accountCity = "Delhi"; // shows dynamically typed language 
+let accountState;
 
-// accountID = 25; not allowed to change const variables
+// accountID = 25; // not allowed to change const variable
 // console.log(accountID)
 
 accountEmail = "def@gmail.com";
 accountPassword = "5678";
+accountCity = "Banglore"
 
-console.table([accountEmail, accountID, accountPassword]);
+console.table([accountEmail, accountID, accountPassword, accountCity, accountState]);
+
+/*
+Note: Prefer not to use var 
+Reason: causes issues in black and function scope
+*/
