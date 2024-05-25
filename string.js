1416b33705168cd4c26ae642ed6edfbc639530f4
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-25 17:04:37 +0530

    string methods

--- string.js
+++ string.js
@@ -1,4 +1,16 @@
 let name = "omkar";
 let hobby = "coding";
 
-console.log(`Hello my name is ${name} and my hobby is ${hobby}`);
+console.log(`Hello my name is ${name} and my hobby is ${hobby}.`);
+
+
+// string methods
+
+const str = new String('Omkar sharma 108')
+console.log(str.length);
+console.log(str.charAt(3));
+console.log(str.split(' '));
+console.log(str.slice(0,4));
+console.log(str.replace(' ', '@'));
+console.log(str.replaceAll(' ', '@'));
+console.log(str.trim());
