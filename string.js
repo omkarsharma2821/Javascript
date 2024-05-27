fcc85d6446070ca6744129a7f5a6bbb05c1ee2b1
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-27 22:52:48 +0530

    String.js file

--- string.js
+++ string.js
@@ -14,7 +14,6 @@ console.log(str.replace(' ', '@'));
 console.log(str.replaceAll(' ', '@'));
 console.log(str.trim());
 
-
 const min = 10;
 const max = 20;
 console.log(Math.floor(Math.random() * (max - min + 1)) + min); 
