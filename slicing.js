5378a891ae972e4359f366d0ddfcae0465898c14
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-30 15:29:03 +0530

    slicing prototype

--- slicing.js
+++ slicing.js
@@ -5,5 +5,5 @@ console.log(animals.length); // length 5
 console.log(animals.slice(2,4)); // [ 'camel', 'duck' ] starting index = 2 , last index = 4
 console.log(animals.slice(1,4)); // [ 'bison', 'camel', 'duck' ]
 console.log(animals.slice(1,3)); // [ 'bison', 'camel' ]
-console.log(animals.slice(2)); // [ 'camel', 'duck', 'elephant' ] here last index in not given
+console.log(animals.slice(2)); // [ 'camel', 'duck', 'elephant' ] starting index  = 2 & here last index in not given
 console.log(animals.slice(-2)); // trick subtract length or array to the indexing array i.e (5-2) = 3
