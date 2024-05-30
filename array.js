9a46d2c71aca5b7896995d4edfcd1e0dcecee817
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-30 15:27:22 +0530

    slicing prototype

--- array.js
+++ array.js
@@ -1,11 +1,13 @@
 const fruits = [];
 fruits.push("banana", "grapes", "mango");
-// fruits.length = 10;
-// console.log(fruits.length);
-// console.log(Object.keys(fruits));
-// console.log(fruits);
+fruits.length = 10;
+console.log(fruits.length);
+console.log(Object.keys(fruits));
+console.log(fruits);
 fruits[5] = "apple";
+
 const iterator = fruits.keys();
 
 for(let key of iterator)
     console.log(`${key} : ${fruits[key]}`);
+
