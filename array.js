d07a487a0edee7a0204afaf1c84da97a682fd80d
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-30 15:16:37 +0530

    loop in js

--- array.js
+++ array.js
@@ -1,6 +1,11 @@
 const fruits = [];
 fruits.push("banana", "grapes", "mango");
-fruits.length = 10;
-console.log(fruits.length);
-console.log(Object.keys(fruits));
-console.log(fruits);
+// fruits.length = 10;
+// console.log(fruits.length);
+// console.log(Object.keys(fruits));
+// console.log(fruits);
+fruits[5] = "apple";
+const iterator = fruits.keys();
+
+for(let key of iterator)
+    console.log(`${key} : ${fruits[key]}`);
