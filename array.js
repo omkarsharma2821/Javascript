deed447f771ca811bcbdb07c2edab09ee4fcb9ed
Omkar Sharma <omkarsharma2821@gmail.com>
2024-06-13 23:31:09 +0530

    array

--- array.js
+++ array.js
@@ -6,7 +6,6 @@ console.log(Object.keys(fruits));
 console.log(fruits);
 fruits[5] = "apple";
 const iterator = fruits.keys();
-
 for(let key of iterator)
     console.log(`${key} : ${fruits[key]}`);
 
