fcd3a235af3646e6a407de56b5d5052fd3c7254b
Omkar Sharma <omkarsharma2821@gmail.com>
2024-06-17 23:36:45 +0530

    NaN example

--- NaN.js
+++ NaN.js
@@ -4,5 +4,5 @@ console.log(typeof score);
 // conversion
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+console.log(typeof valueInNumber); // data type number
 console.log(valueInNumber); // the value in this is NaN 
