59f94fa68e360133f9ac472e454bd31e61c926fa
Omkar Sharma <omkarsharma2821@gmail.com>
2024-06-19 23:15:19 +0530

    NaN

--- NaN.js
+++ NaN.js
@@ -2,7 +2,6 @@ let score = "33abc"
 console.log(typeof score);
 
 // conversion
-
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber); // data type number
 console.log(valueInNumber); // the value in this is NaN 
