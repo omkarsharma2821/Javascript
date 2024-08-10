cf4f6831697ecb53d2a8d5225b6a1fcd5ace8a7d
Omkar Sharma <omkarsharma2821@gmail.com>
2024-08-10 14:37:34 +0530

    operations

---
+++ operations.js
@@ -0,0 +1,6 @@
+console.log(1 + 2 + "3"); // 33
+console.log("3" + 1 + 2); // 312
+console.log(1 + "3" + 2); // 132
+
+// this type of code is not appropriate for the production code.
+// this is just for the understanding of the code and solving MCQ.
