41e7671b7985fd59e27ad12fca2b3d4cf7ffb18d
Omkar Sharma <omkarsharma2821@gmail.com>
2024-08-10 15:06:05 +0530

    data types in js

--- datatypes.js
+++ datatypes.js
@@ -1,9 +1,28 @@
-// null => standalone value
-// undefined => value is not assigned
-// number
-// string
-// boolean
-// obejct
+// Datatypes in JavaScript
+
+// JavaScript is a loosely typed language, which means you don't have to declare the type of a variable when you declare it. The type will be determined dynamically at runtime.
+
+//example of dynamic type language
+
+// let x = 5; // x is an integer
+// let x = "Hello"; // x is a string
+
+// primitive data types
+
+// null => object
+// undefined => undefined
+// Number => number
+// String => string
+// Boolean => boolean
+// BigInt => bigint
+// Symbol => symbol
+
+// non-primitive data types
+
+// Object => object
+// Array => object
+// Function => function
+
 
 let age = null;
 let name;
