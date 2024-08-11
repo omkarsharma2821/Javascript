bda95ea635efe6717bdd625b5eb5474dffbf0a99
Omkar Sharma <104587783+omkarsharma2821@users.noreply.github.com>
2024-08-11 16:32:32 +0530

    Update splice.js

--- splice.js
+++ splice.js
@@ -7,5 +7,5 @@ console.log(animals.splice(1,3)); // [ 'bison', 'camel', 'duck' ]
 console.log(animals); // [ 'ant', 'elephant' ]
 
 // difference b/w slice and splice is 
-// splice update(trim) the original array and it also includes the last range
+// splice update change (trim) the original array
 // Last range is also included in the array.
