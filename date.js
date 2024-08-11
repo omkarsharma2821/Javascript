ded5be97d7011b1aa6b51d0f40950784b7f41d6b
Omkar Sharma <omkarsharma2821@gmail.com>
2024-08-11 15:59:06 +0530

    date in js

---
+++ date.js
@@ -0,0 +1,10 @@
+// dates painpoint of JS
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
+
+
