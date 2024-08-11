db675482178cc2ed57ed5532be9c16368bfad234
Omkar Sharma <omkarsharma2821@gmail.com>
2024-08-11 16:28:44 +0530

    splice and slice

--- splice.js
+++ splice.js
@@ -1,6 +1,11 @@
 const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
-console.log(animals.splice(2,4));
-console.log(animals);
 
-// Main difference b/w slice and splice is splice update(trim) the original array 
+console.log(animals.slice(1,3)); // [ 'bison', 'camel' ]
+console.log(animals); // [ 'ant', 'bison', 'camel', 'duck', 'elephant' ]
+
+console.log(animals.splice(1,3)); // [ 'bison', 'camel', 'duck' ]
+console.log(animals); // [ 'ant', 'elephant' ]
+
+// difference b/w slice and splice is 
+// splice update(trim) the original array and it also includes the last range
 // Last range is also included in the array.
