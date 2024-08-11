7f7f74438f11bb08670ab397b22a9009bffd8240
Omkar Sharma <omkarsharma2821@gmail.com>
2024-08-11 22:07:50 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,15 @@
+const mySym = Symbol("Key1");
+
+const user ={
+    name: 'Omkar',
+    age: 30,
+    email: "omkar@gmail.com",
+    location: 'Pune', 
+    mySym: "Key1", 
+}
+
+console.log(user["email"]);
+console.log(user.name);
+console.log(user.mySym);
+
+
