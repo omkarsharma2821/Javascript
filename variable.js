16d30b9f1ba287443d8528ed66a6dc8c86795f56
Omkar Sharma <omkarsharma2821@gmail.com>
2024-08-21 11:38:01 +0530

    scope

--- variable.js
+++ variable.js
@@ -16,5 +16,16 @@ console.table([accountEmail, accountID, accountPassword, accountCity, accountSta
 /*
 Note: Prefer not to use var 
 Reason: causes issues in black and function scope
-var have function scope and let, const have block scope
+var have function scope ,let and const have block scope
 */
+
+
+// problem appears when multiple coder are there and using var keyword which will lead to problems
+if (true){  // this if block is a block scope & above variables will in global scope
+    var a = 10;
+    let b = 20;
+    const c = 30;
+}
+console.log(a); // 10
+console.log(b); // ReferenceError: b is not defined
+console.log(c); // ReferenceError: c is not defined
