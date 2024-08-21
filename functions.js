b7c50e33909988dc8b0c431e3a15ea06ab45c73a
Omkar Sharma <omkarsharma2821@gmail.com>
2024-08-21 11:57:15 +0530

    closure and scope

--- functions.js
+++ functions.js
@@ -2,4 +2,16 @@ function addTwoNumbers(a, b) {
   return a + b;
 }
 const result = addTwoNumbers(1, 2); // 3
-console.log(result);
+// console.log(result);
+
+function one(){ // example ice-cream cone child can take cone from elder but elder can't take cone from child
+  const name = "John";
+
+  function two(){
+    const age = 20;
+    console.log(name);
+  }
+  // console.log(age); // ReferenceError: age is not defined 
+  two(); // John 
+}
+one();
