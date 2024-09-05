23a9a29971061637be73f0b31eef2c18f9689edd
Omkar Sharma <omkarsharma2821@gmail.com>
2024-09-05 12:47:10 +0530

    Add Nullish Coalescing Operator

--- truthy.js
+++ truthy.js
@@ -5,3 +5,13 @@
 //truthy values
 
 // "0", 'false', " ", [], {}, function(){}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10 // 5
+// val1 = null ?? 10 //10
+// val1 = undefined ?? 15 // 15 
+val1 = null ?? 10 ?? 20 // 10
+
+console.log(val1);
